9b37cb6d138a70033f0e70dd1b0268259cd0589b
kapalaq <mainmailformail@gmail.com>
2024-05-07 13:43:08 +0500

    Third task, completed everything

--- nfactorialWeb/settings.py
+++ nfactorialWeb/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-hblj@5u*_aai01iau#bz5=65cn#i*)r16*h@9wnexi9oz==zmq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1']
 
